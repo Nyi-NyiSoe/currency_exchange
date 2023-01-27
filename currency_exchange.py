616efafd1194421ef723af8be360f316cc57b871
Nyi Nyi Soe <jackemrick2k@gmail.com>
2023-01-27 17:31:11 +0630

    slight change in design / added button

--- currency_exchange.py
+++ currency_exchange.py
@@ -26,6 +26,20 @@ img2 = PIL.Image.open("images/thai.png")
 img_var2 = ImageTk.PhotoImage(img2)
 Label(frame,relief=SUNKEN,image=img_var2).grid(row=1,column=3)
 
+Label(frame,font=20,width=15,text='1',relief=SUNKEN).grid(row=2,column=0,padx=20,pady=20)
+
+ex_rate=0
+Label(frame,font=20,width=15,text=ex_rate,relief=SUNKEN).grid(row=2,column=3,padx=20,pady=20)
+
+
+result=0
+Label(frame,font=20,width=15,text=result,relief=SUNKEN).grid(row=3,columnspan=6,padx=20,pady=20)
+
+Label(frame,font=20,width=15,text="Result ==>").grid(row=3,column=0,padx=20,pady=20)
+
+
+Button(frame,text="Calculate",command=None,width=30).grid(row=4,columnspan=6)
+
 
 images = {""}
 root.mainloop()
